c62e432496e0962098116aca22667026a15e2dad
anujjoshi <anuj@groktor.net>
2020-10-24 18:28:01 +0200

    Problem solved

--- ProblemSolving- DinDinn/ViewController.swift
+++ ProblemSolving- DinDinn/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let value: Int = 122
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        measure(string: "abc", number: 2)
+        measure(string: "abc", number: 28)
     }
 
+    func measure(string: String, number: Int) {
+        var inputString: String = ""
+
+        for index in string.unicodeScalars {
+            let asciiValue = Int(index.value)
+
+            var duplicateNumber = number
+
+            if asciiValue + number > value {
+                duplicateNumber -= (value - asciiValue)
+                duplicateNumber = duplicateNumber % 26
+                inputString += "\(Character(UnicodeScalar(UInt8(96 + duplicateNumber))))"
+            } else {
+                inputString += "\(Character(UnicodeScalar(asciiValue + duplicateNumber)!))"
+            }
+        }
+        print(inputString)
+    }
 
 }
 
